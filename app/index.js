aa782ea7f4966f5bcf9581f44cc9b02f55a8c5d6
jkuo630 <89288700+jkuo630@users.noreply.github.com>
2023-05-31 22:10:29 -0400

    no api access, completed version

--- app/index.js
+++ app/index.js
@@ -1,34 +1,5 @@
-import { useState } from "react";
-import { View, ScrollView, SafeAreaView } from "react-native";
-import { Stack, useRouter } from "expo-router";
+import { Redirect } from "expo-router";
 
-import { COLORS, icons, images, SIZES } from "../constants";
-import {
-  Nearbyjobs,
-  Popularjobs,
-  ScreenHeaderBtn,
-  Welcome,
-} from "../components";
-
-const Home = () => {
-  const router = useRouter();
-
-  return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
-      <Stack.screen
-        options={{
-          headerStyle: { backgroundColor: COLORS.lightWhite },
-          headerShadowVisible: false,
-          headerLeft: () => (
-            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
-          ),
-          headerRight: () => (
-            <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
-          ),
-        }}
-      />
-    </SafeAreaView>
-  );
-};
-
-export default Home;
+export default function Index() {
+    return <Redirect href="/home" />;
+}
